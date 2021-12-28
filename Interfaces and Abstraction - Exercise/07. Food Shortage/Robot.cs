40a1f8e6f6e8e906135359a5913753dfb4e0733a
Philipova <75500722+Philipova@users.noreply.github.com>
2021-12-28 10:32:34 +0200

    Create Robot.cs

---
+++ Interfaces and Abstraction - Exercise/07. Food Shortage/Robot.cs
@@ -0,0 +1,17 @@
+namespace FoodShortage
+{
+    using Interfaces;
+
+    public class Robot : IIdentifiable
+    {
+        private string model;
+
+        public Robot(string model, string id)
+        {
+            this.model = model;
+            this.Id = id;
+        }
+
+        public string Id { get; private set; }
+    }
+}
