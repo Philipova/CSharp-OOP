2177e6f35985b07469d5977aca844d581354ca38
Philipova <75500722+Philipova@users.noreply.github.com>
2022-01-02 12:40:25 +0200

    Create IResident.cs

---
+++ Interfaces and Abstraction - Exercise/10. Explicit Interfaces/Interfaces/IResident.cs
@@ -0,0 +1,11 @@
+namespace ExplicitInterfaces.Interfaces
+{
+    public interface IResident
+    {
+        string Name { get; }
+
+        string Country { get; }
+
+        string GetName();
+    }
+}
