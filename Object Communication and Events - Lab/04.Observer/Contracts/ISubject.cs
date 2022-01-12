3594d99463ce3663d8d6bcd37a21ee1631c1b4ab
Philipova <75500722+Philipova@users.noreply.github.com>
2022-01-12 09:50:15 +0200

    Create ISubject.cs

---
+++ Object Communication and Events - Lab/04.Observer/Contracts/ISubject.cs
@@ -0,0 +1,11 @@
+namespace Heroes.Contracts
+{
+    public interface ISubject
+    {
+        void Register(IObserver observer);
+
+        void Unregister(IObserver observer);
+
+        void NotifyObservers();
+    }
+}
