b05f0c1c6130d5a0b6f7a825fb05d165f9730c52
Philipova <75500722+Philipova@users.noreply.github.com>
2022-01-14 10:50:28 +0200

    Create Car.cs

---
+++ Polymorphism - Exercise/01. Vehicles/Car.cs
@@ -0,0 +1,9 @@
+namespace Vehicles
+{
+    class Car : Vehicle
+    {
+        public Car(double fuelQuantity, double fuelConsumtion) 
+            : base(fuelQuantity, fuelConsumtion + 0.9)
+        {
+        }
+    }
