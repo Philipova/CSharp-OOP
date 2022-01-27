35e8087e5d8f57d855ff0091dfea7da06b6587cc
Philipova <75500722+Philipova@users.noreply.github.com>
2022-01-27 13:46:22 +0200

    Create IGem.cs

---
+++ Reflection and Attributes - Exercise/07. Inferno Infinity/Contracts/IGem.cs
@@ -0,0 +1,11 @@
+namespace _07._Inferno_Infinity.Contracts
+{
+    public interface IGem
+    {
+        int Strength { get; }
+
+        int Agility { get; }
+
+        int Vitality { get; }
+    }
+}
