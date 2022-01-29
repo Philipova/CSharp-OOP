a47f24a220df3315e33f00bb04c520ce258e1355
Philipova <75500722+Philipova@users.noreply.github.com>
2022-01-29 16:44:31 +0200

    Create StartUp.cs

---
+++ Reflection and Attributes - Lab/01. Stealer/StartUp.cs
@@ -0,0 +1,12 @@
+using System;
+
+public class StartUp
+{
+    public static void Main()
+    {
+        Spy spy = new Spy();
+        string result = spy.StealFieldInfo($"Hacker", "username", "password");
+
+        Console.WriteLine(result);
+    }
+}
