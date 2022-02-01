9dff370af25ee3a5f37e690ceb8c9f5646e67e96
Philipova <75500722+Philipova@users.noreply.github.com>
2022-02-01 10:10:50 +0200

    Create StartUp.cs

---
+++ Reflection and Attributes - Lab/04. Collector/StartUp.cs
@@ -0,0 +1,12 @@
+using System;
+
+public class StartUp
+{
+    public static void Main()
+    {
+        Spy spy = new Spy();
+        string result = spy.CollectGettersAndSetters($"Hacker");
+
+        Console.WriteLine(result);
+    }
+}
