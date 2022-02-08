21237c2b9fe73a3d5854b874fb21c3fe3bba2191
Philipova <75500722+Philipova@users.noreply.github.com>
2022-02-08 17:28:47 +0200

    Create StartUp.cs

---
+++ SOLID - Lab/03.DetailPrinter/StartUp.cs
@@ -0,0 +1,13 @@
+namespace _03.DetailPrinter
+{
+    using _03.DetailPrinter.Contarcts;
+    using System;
+    using System.Collections.Generic;
+
+    class StartUp
+    {
+        static void Main()
+        {
+        }
+    }
+}
