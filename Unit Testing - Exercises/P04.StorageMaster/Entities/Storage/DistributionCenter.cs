728c3170dd9456ceb48bd8b9657f5773167ed6d3
Philipova <75500722+Philipova@users.noreply.github.com>
2022-02-15 21:15:09 +0200

    Create DistributionCenter.cs

---
+++ Unit Testing - Exercises/P04.StorageMaster/Entities/Storage/DistributionCenter.cs
@@ -0,0 +1,17 @@
+namespace P04.StorageMaster.Entities.Storage
+{
+	using Vehicles;
+
+	public class DistributionCenter : Storage
+	{
+		private static readonly Vehicle[] DefaultVehicles =
+		{
+			new Van(), new Van(), new Van()
+		};
+
+		public DistributionCenter(string name)
+			: base(name, capacity: 2, garageSlots: 5, vehicles: DefaultVehicles)
+		{
+		}
+	}
+}
